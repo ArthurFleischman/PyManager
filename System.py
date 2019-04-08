1d109895d78b083552d50681c645c5b0e170b2bc
Arthur Fleischman <cabralfle@gmail.com>
2019-04-08 17:29:39 -0300

    functional script python + mysql

--- System.py
+++ System.py
@@ -1,6 +1,3 @@
-from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow,QLabel
-import sys
 import mysql.connector
 
 class Mysql():
@@ -23,37 +20,9 @@ class Mysql():
         result = self.mycursor.fetchall()
         return result
 
-
-class Window(QMainWindow):
-
-    def __init__(self):
-        super().__init__()
-
-        self.title = 'PymManager'
-        self.top = 400
-        self.left = 400
-        self.width = 400
-        self.height = 400
-        self.icon = "icon path"
-        lbl = QLabel('hello world', self)
-        lbl.move(0,0)
-        self.initwindow()
-
-    def initwindow(self):
-        self.setWindowIcon(QtGui.QIcon(self.icon))
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.top, self.left, self.width, self.height)
-        self.show()
-
-
-
 if __name__ == '__main__':
 
     db = Mysql('localhost','user','password')
     db.usedatabase('database')      #use especific database
-    val = db.select('*','clients')  #get all data from clients
+    val = db.select('*','clients')  #get all data from clients or other stuff
     print(val)                      #print all data
-
-    app = QApplication(sys.argv)
-    window = Window()
-    sys.exit(app.exec_())
